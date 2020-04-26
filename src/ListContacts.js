9f221f40c860d5ec8f7bcbeeb2767dfef51a8304
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-04-26 08:49:32 +0530

    Changed contact file according to styling

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -7,10 +7,23 @@ class ListContacts extends Component {
     return (
       <ol className='contact-list'>
 
-     { this.props.contacts.map( (contact) =>
-                 	<li key={contact.id}>{contact.id}</li>
-                 	)
+     { this.props.contacts.map( (contact) =>(
+                        <li key={contact.id} className="contact-list-item">
+                       <div className='contact-avatar'
+                       style={{
+                       backgroundImage:`url(${contact.avatarURL})`
+                      }}
+                      >
+                      </div>
+                      <div className="contact-details">
+                      <p>{contact.name}</p>
+                      <p>{contact.handle}</p>
+                      </div>
+                      <button className="contact-remove">Remove</button>
+                      </li>)
+                 )
                  }
+
       </ol>
     )
   }
