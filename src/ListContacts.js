01091b7e753e41b22b0c554d31b63c47e600d844
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-04-29 21:05:38 +0530

    Added search bar

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -3,9 +3,42 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class ListContacts extends Component {
+
+  static propTypes =
+  {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+
+  state={
+    query:''
+  }
+
+  updateQuery=(query)=>{
+    this.setState(() =>({
+      query: query.trim()
+    }))
+  
+  }
+
+
   render() {
     console.log('Props', this.props)
+    
     return (
+      <div className='list-contacts'>
+      {JSON.stringify(this.state)}
+      <div className='list-contacts-top'>
+      <input
+      className='search-contacts'
+      type='text'
+      placeholder='Search Contacts'
+      value={this.state.query}
+      onChange={(event)=>
+        this.updateQuery(event.target.value)}/>
+      </div>
+
+
       <ol className='contact-list'>
 
      { this.props.contacts.map( (contact) =>(
@@ -28,14 +61,11 @@ class ListContacts extends Component {
                  }
 
       </ol>
+      </div>
     )
   }
 }
-  ListContacts.propTypes =
-  {
-    contacts: PropTypes.array.isRequired,
-    onDeleteContact: PropTypes.func.isRequired
-  }
+  
 
 
 export default ListContacts 
