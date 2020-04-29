e49b66dd1d4ff0bac7dee3a276d6e2ff8cfc04df
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-04-29 12:11:05 +0530

    Remove contacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -19,7 +19,9 @@ class ListContacts extends Component {
                       <p>{contact.name}</p>
                       <p>{contact.handle}</p>
                       </div>
-                      <button className="contact-remove">Remove</button>
+                      <button 
+                      onClick={()=>this.props.onDeleteContact(contact)}
+                      className="contact-remove">Remove</button>
                       </li>)
                  )
                  }
